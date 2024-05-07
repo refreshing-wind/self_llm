ad74941e414f05385afa8b10560dd4a4ecf40ad0
refreshing-wind <1139859938@qq.com>
2024-05-07 11:27:22 +0800

    新建项目

---
+++ README.md
@@ -0,0 +1,5 @@
+# 手写LLM预训练和微调框架
+## TODO
+1. SFT
+2. pretrain
+3. RLHF
---
+++ sft/sft.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+'''
+ Author       : Xuexin
+ Date         : 2024-05-07 10:32:43
+ LastEditTime : 2024-05-07 10:32:49
+ FilePath     : \\self_llm\\sft\\sft.py
+ Description  : 
+'''
